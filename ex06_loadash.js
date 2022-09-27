17325652133a44d3bf1f50fb608c0c72e8c3ddf6
Guilherme <mttsguil@gmail.com>
2022-09-27 09:25:31 -0300

    add: extern module - lodash

---
+++ ex06_loadash.js
@@ -0,0 +1,11 @@
+const _ = require('lodash')
+
+const alunos = [
+    {name: 'João', nota: 7.6},
+    {name: 'Maria', nota: 8.6},
+    {name: 'Pedro', nota: 8.1}
+]
+
+const media = _.sumBy(alunos, 'nota') / alunos.length
+
+console.log(media)
